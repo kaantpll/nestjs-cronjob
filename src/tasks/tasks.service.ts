306b4cc05768f11b5acf804a2750a047c4b52076
kaan topal <kaant43@gmail.com>
2023-04-17 19:55:29 +0300

    task scheduler cron job

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,12 @@
 import { Injectable, Logger } from "@nestjs/common";
+import { Cron } from "@nestjs/schedule";
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
 
+  @Cron('* * * * * *',{name:"Clean student database each day"})
+  handleCron() {
+    this.logger.debug('Called when the current second is 45');
+  }
 }
